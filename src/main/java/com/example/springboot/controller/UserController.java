6b5f6850e1f66f77f25c58c69fb9aafb512d265f
sfedo <sfedor2000@gmail.com>
2023-01-11 23:29:57 +0600

    1st commit

---
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.User;
+import com.example.springboot.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String showUsers(Model model) {
+        model.addAttribute("users", userService.showUsers());
+        return "users";
+    }
+    @GetMapping("/addnew")
+    public String addNewUser(User user) {
+        return "adduser";
+    }
+
+    @PostMapping()
+    public String createUserForm(User user) {
+        userService.add(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String updateUserForm(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.showUser(id));
+        return "edituser";
+    }
+    @PatchMapping("/{id}")
+    public String updateUser(User user) {
+        userService.add(user);
+        return "redirect:/";
+    }
+    @DeleteMapping("/{id}/delete")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.delete(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/springboot/model/User.java
@@ -0,0 +1,20 @@
+package com.example.springboot.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "lastname")
+    private String lastname;
+    @Column(name = "age")
+    private int age;
+}
